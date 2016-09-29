ffb0bc8aca3abfa8be0b32cd17af1c4518982105
Hou Jue <jxhou@da5-cs-bk107-26.helsinki.fi>
2016-09-29 11:37:08 +0300

    Sept 29

--- lamport-master.py
+++ lamport-master.py
@@ -1,20 +1,22 @@
 #-*- coding: UTF-8 -*-
-import os,sys,linecache,random,socket,_thread,time
+import sys,linecache,socket,_thread,time
 
-configs = linecache.getlines(sys.argv[1])
+
 
 
 def beat(port):
 	print(port)
 	s = socket.socket()
 	if s.connect_ex((socket.gethostname(),port)) == 0:
+		print(port)
 		s.send(bytes('start','UTF-8'))
 	s.close()
 
-
 i = 0
+configs = linecache.getlines(sys.argv[1])
 while i < len(configs):
-	para = int(configs[i].split(' ')[1])
+	para = int(configs[i].split(' ')[1].strip())
 	_thread.start_new_thread(beat,(para,))
 	i += 1
-
+	
+time.sleep(20)
