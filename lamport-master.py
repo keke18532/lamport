63e41cdc149183db8300e38da15fcf76dda9d39b
Hou Jue <jxhou@da5-cs-bk107-26.helsinki.fi>
2016-09-29 11:42:28 +0300

    Sept 29

--- lamport-master.py
+++ lamport-master.py
@@ -5,10 +5,8 @@ import sys,linecache,socket,_thread,time
 
 
 def beat(port):
-	print(port)
 	s = socket.socket()
 	if s.connect_ex((socket.gethostname(),port)) == 0:
-		print(port)
 		s.send(bytes('start','UTF-8'))
 	s.close()
 
@@ -19,4 +17,4 @@ while i < len(configs):
 	_thread.start_new_thread(beat,(para,))
 	i += 1
 	
-time.sleep(20)
+time.sleep(int(len(configs)*0.7))
